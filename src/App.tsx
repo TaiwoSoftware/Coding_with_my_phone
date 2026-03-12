4f99d9186fd1276071166cf7d783f0b4e2788ac4
engineerAkerele <davcodes24@gmail.com>
2026-03-12 12:10:00 +0100

    trying to import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
+  const handleClick = () => {
+    setCount(count + 1)
+  }
   return (
     <>
-      <h1>Hi guys is this working</h1>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>Increase</button>
+       <input type="text" placeholder="Your name" />
     </>
   )
 }
-
 export default App
